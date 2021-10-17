5a7be4b7041f3ad4b62687d283d0c1e4b6196c02
hakankaraahmet <karaahmethkn@gmail.com>
2021-10-17 13:07:32 +0300

    components wrapped by themeProvider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import TypographyLesson from "./Lessons/TypographyLesson"
 import ButtonLesson from "./Lessons/ButtonLesson";
 import IconLesson from "./Lessons/IconLesson";
+import MakeStyleHook from "./Lessons/MakeStyleHook";
+import CustomThemes from "./Lessons/CustomThemes";
+import {ThemeProvider} from "@mui/material"
+import {theme} from "./Theme/Theme"
+
 const App = () => {
   return (
     <div>
+      <ThemeProvider theme={theme}>
       <TypographyLesson/> 
       <ButtonLesson/>
       <IconLesson/>
+      <MakeStyleHook/>
+      <CustomThemes/>
+      </ThemeProvider>
     </div>
   );
 };
