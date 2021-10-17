f245c525e0fc7e08f78f623133bd6d6300a61d0f
hakankaraahmet <karaahmethkn@gmail.com>
2021-10-17 17:26:51 +0300

    modified app.js with new components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from "react";
-import TypographyLesson from "./Lessons/TypographyLesson"
+import TypographyLesson from "./Lessons/TypographyLesson";
 import ButtonLesson from "./Lessons/ButtonLesson";
 import IconLesson from "./Lessons/IconLesson";
 import MakeStyleHook from "./Lessons/MakeStyleHook";
 import CustomThemes from "./Lessons/CustomThemes";
-import {ThemeProvider} from "@mui/material"
-import {theme} from "./Theme/Theme"
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./Theme/Theme";
+import TextFields from "./Lessons/TextFields";
+import RadioButtons from "./Lessons/RadioButtons";
+import GridSystem from "./Lessons/GridSystem";
+import StyledItem from "./Lessons/StyledItem"
+
 
 const App = () => {
   return (
     <div>
       <ThemeProvider theme={theme}>
-      <TypographyLesson/> 
-      <ButtonLesson/>
-      <IconLesson/>
-      <MakeStyleHook/>
-      <CustomThemes/>
+        <TypographyLesson />
+        <ButtonLesson />
+        <IconLesson />
+        <MakeStyleHook />
+        <CustomThemes /> 
+        <TextFields/>
+        <RadioButtons/> 
+        <GridSystem/>
+        <StyledItem/>
       </ThemeProvider>
     </div>
   );
